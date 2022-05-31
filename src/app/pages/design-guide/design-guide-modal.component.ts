4af33f7ef0a0fa6171dd239028bf439b3da575fe
Rubin Hu <rubin_hu@wistron.com>
2022-05-31 16:43:07 +0800

    Update two demos for modal.

--- src/app/pages/design-guide/design-guide-modal.component.ts
+++ src/app/pages/design-guide/design-guide-modal.component.ts
@@ -5,9 +5,44 @@ import { NzModalService } from 'ng-zorro-antd/modal';
   selector: 'design-guide-modal',
   template: ` 
     <h1>Modal</h1> 
-    <button nz-button [nzType]="'primary'" (click)="showModal()"><span>Show Modal</span></button>
-    <nz-modal [(nzVisible)]="isVisible" nzTitle="The first Modal" (nzOnCancel)="handleCancel()" (nzOnOk)="handleOk()">
+    <button nz-button [nzType]="'primary'" (click)="showNormalModal()"><span>Show Normal Modal</span></button>
+    <nz-modal [(nzVisible)]="isNarmalVisible" nzTitle="The Normal Modal (top: 100)" (nzOnCancel)="handleCancel()" (nzOnOk)="handleOk()">
       <ng-container *nzModalContent>
+        <p>不設置 modal 的高度，將打開一個默認窗口，位置(top: 100)</p>
+        <p>Content one</p>
+        <p>Content two</p>
+        <p>Content three</p>
+      </ng-container>
+    </nz-modal>
+    <button nz-button [nzType]="'primary'" (click)="showLargeModal()"><span>Show Large Modal</span></button>
+    <nz-modal [(nzVisible)]="isLargeVisible" nzTitle="The Large Modal ({height:'100vh', top:0})" (nzOnCancel)="handleCancel()" (nzOnOk)="handleOk()" [nzWidth]="1000" [nzStyle]="{height:'100vh', top:0}">
+      <ng-container *nzModalContent>
+        <p>打開一個較大的窗口，需要設置窗口的高度和位置(height:'100vh', top:0)，按用戶電腦高度顯示</p>
+        <p>Modal对话框
+          模态对话框。
+          何时使用#
+          需要用户处理事务，又不希望跳转页面以致打断工作流程时，可以使用 Modal 在当前页面正中打开一个浮层，承载相应的操作。
+          另外当需要一个简洁的确认框询问用户时，可以使用精心封装好的 NzModalService.confirm() 等方法。
+          推荐使用加载Component的方式弹出Modal，这样弹出层的Component逻辑可以与外层Component完全隔离，并且做到可以随时复用，
+          在弹出层Component中可以通过依赖注入NzModalRef方式直接获取模态框的组件实例，用于控制在弹出层组件中控制模态框行为。</p>
+        <p>Content two</p>
+        <p>Content three</p>
+        <p>Content one</p>
+        <p>Content two</p>
+        <p>Content three</p> <p>Content three</p>
+        <p>Content one</p>
+        <p>Content two</p>
+        <p>Content three</p>
+        <p>Content three</p>
+        <p>Content one</p>
+        <p>Content two</p>
+        <p>Content three</p>
+        <p>Content one</p>
+        <p>Content two</p>
+        <p>Content three</p>
+        <p>Content one</p>
+        <p>Content two</p>
+        <p>Content three</p>
         <p>Content one</p>
         <p>Content two</p>
         <p>Content three</p>
@@ -23,7 +58,8 @@ import { NzModalService } from 'ng-zorro-antd/modal';
     `]
 })
 export class DesignGuideModalComponent implements OnInit {
-  isVisible = false;
+  isNarmalVisible = false;
+  isLargeVisible = false;
 
   constructor(private modal: NzModalService) { 
     console.log("DesignGuideModalComponent","constructor");
@@ -33,18 +69,24 @@ export class DesignGuideModalComponent implements OnInit {
     console.log("DesignGuideModalComponent","ngOnInit");
   }
 
-  showModal(): void {
-    this.isVisible = true;
+  showNormalModal(): void {
+    this.isNarmalVisible = true;
+  }
+
+  showLargeModal(): void {
+    this.isLargeVisible = true;
   }
 
   handleOk(): void {
     console.log('Button ok clicked!');
-    this.isVisible = false;
+    this.isNarmalVisible = false;
+    this.isLargeVisible = false;
   }
 
   handleCancel(): void {
     console.log('Button cancel clicked!');
-    this.isVisible = false;
+    this.isNarmalVisible = false;
+    this.isLargeVisible = false;
   }
 
   showConfirm(): void {
